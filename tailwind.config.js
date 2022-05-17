bf03c0800f1474cb5c22e02fe41d419e3aaa87aa
milcab <milcarodriguez23@gmail.com>
2022-05-16 23:20:53 -0400

    adding tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "blue-dark": "#2b3e68",
+        "blue-medium": "#577bbe",
+        "blue-light": "#6386c4",
+        gold: "#c5ad4a",
+        white: "#ebe7ec"
+      }
+    },
+  },
+  plugins: [],
+}
