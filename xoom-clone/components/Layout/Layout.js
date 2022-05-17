1307c5024947797cb1b42ba16df44dfa3ecb99d5
milcab <milcarodriguez23@gmail.com>
2022-05-16 21:37:06 -0400

    remove misplaced component

--- xoom-clone/components/Layout/Layout.js
+++
@@ -1,9 +0,0 @@
-const Layout = ({ children }) => {
-    return (<div>
-        <div>nav bar here</div>
-        {children}
-        <div>footer bar here</div>
-    </div>)
-}
-
-export default Layout;
