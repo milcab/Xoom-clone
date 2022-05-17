89fb76ecdc5e64b657b25c9979f4680c1d1d9a3e
milcab <milcarodriguez23@gmail.com>
2022-05-16 23:32:01 -0400

    add scroll to list of countries

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -66,7 +66,7 @@ const Dropdown = ({ title = "Send Money", dropdownOptions = [] }) => {
           >
             <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
               <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8 overflow-scroll max-h-[150px]">
                   {dropdownOptions.map((item) => (
                     <a
                       key={item.name}
