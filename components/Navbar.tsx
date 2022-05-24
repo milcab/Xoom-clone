e2b0349f831db3bcef2dd3462713deae8e5f6df6
milcab <milcarodriguez23@gmail.com>
2022-05-24 14:58:24 -0400

    mobile signout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -125,78 +125,94 @@ const SubMenuOptions = () => (
   </Popover.Group>
 );
 
-const MobileMenu = () => (
-  <Transition
-    as={Fragment}
-    enter="duration-200 ease-out"
-    enterFrom="opacity-0 scale-95"
-    enterTo="opacity-100 scale-100"
-    leave="duration-100 ease-in"
-    leaveFrom="opacity-100 scale-100"
-    leaveTo="opacity-0 scale-95"
-  >
-    <Popover.Panel
-      focus
-      className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+const MobileMenu = () => {
+  const { authUser, signOut } = useFirebaseAuth();
+  return (
+    <Transition
+      as={Fragment}
+      enter="duration-200 ease-out"
+      enterFrom="opacity-0 scale-95"
+      enterTo="opacity-100 scale-100"
+      leave="duration-100 ease-in"
+      leaveFrom="opacity-100 scale-100"
+      leaveTo="opacity-0 scale-95"
     >
-      <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-        <div className="pt-5 pb-6 px-5">
-          <div className="flex items-center justify-between">
-            <div>
-              <Link href="/">
-                <img
-                  className="h-10 w-auto"
-                  src="/logos/white_logo_transparent_background.png"
-                  alt="Workflow"
-                />
-              </Link>
+      <Popover.Panel
+        focus
+        className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+      >
+        <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+          <div className="pt-5 pb-6 px-5">
+            <div className="flex items-center justify-between">
+              <div>
+                <Link href="/">
+                  <img
+                    className="h-10 w-auto"
+                    src="/logos/white_logo_transparent_background.png"
+                    alt="Workflow"
+                  />
+                </Link>
+              </div>
+              <div className="-mr-2">
+                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="sr-only">Close menu</span>
+                  <XIcon className="h-6 w-6" aria-hidden="true" />
+                </Popover.Button>
+              </div>
             </div>
-            <div className="-mr-2">
-              <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                <span className="sr-only">Close menu</span>
-                <XIcon className="h-6 w-6" aria-hidden="true" />
-              </Popover.Button>
-            </div>
-          </div>
-          <div className="mt-6">
-            <nav className="grid gap-y-8">
-              {COUNTRIES.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                >
-                  {/* <item.icon
+            <div className="mt-6">
+              <nav className="grid gap-y-8">
+                {COUNTRIES.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                  >
+                    {/* <item.icon
                     className="flex-shrink-0 h-6 w-6 text-indigo-600"
                     aria-hidden="true"
                   /> */}
-                  <span className="ml-3 text-base font-medium text-gray-900">
-                    {item.name}
-                  </span>
+                    <span className="ml-3 text-base font-medium text-gray-900">
+                      {item.name}
+                    </span>
+                  </a>
+                ))}
+              </nav>
+            </div>
+          </div>
+          <div className="py-6 px-5 space-y-6">
+            <div>
+              {authUser ? (
+                <a
+                  onClick={signOut}
+                  className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                >
+                  Log out
                 </a>
-              ))}
-            </nav>
+              ) : (
+                <>
+                  <Link href="/signup">
+                    <a className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+                      Sign up
+                    </a>
+                  </Link>
+                  <p className="mt-6 text-center text-base font-medium text-gray-500">
+                    Existing customer?{" "}
+                    <Link href="/signin">
+                      <a className="text-indigo-600 hover:text-indigo-500">
+                        Sign in
+                      </a>
+                    </Link>
+                  </p>
+                </>
+              )}
+            </div>
           </div>
         </div>
-        <div className="py-6 px-5 space-y-6">
-          <div>
-            <Link href="/signup">
-              <a className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
-                Sign up
-              </a>
-            </Link>
-            <p className="mt-6 text-center text-base font-medium text-gray-500">
-              Existing customer?{" "}
-              <Link href="/signin">
-                <a className="text-indigo-600 hover:text-indigo-500">Sign in</a>
-              </Link>
-            </p>
-          </div>
-        </div>
-      </div>
-    </Popover.Panel>
-  </Transition>
-);
+      </Popover.Panel>
+    </Transition>
+  );
+};
 
 const RightMenu = () => {
   const { authUser, signOut } = useFirebaseAuth();
@@ -207,7 +223,7 @@ const RightMenu = () => {
           onClick={signOut}
           className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
         >
-          Logout
+          Log out
         </a>
       </div>
     );
