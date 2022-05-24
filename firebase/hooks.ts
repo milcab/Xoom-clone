3003566e60c89ee0b5c4aa7ad39e8975ed56e206
milcab <milcarodriguez23@gmail.com>
2022-05-24 15:05:59 -0400

    update hooks

--- firebase/hooks.ts
+++ firebase/hooks.ts
@@ -1,36 +1,64 @@
-import { useState, useEffect } from 'react'
-import {auth} from './app';
+import { useState, useEffect } from "react";
+import { auth, emailVerification, signUp, signIn, updateUser } from "./app";
 
 const formatAuthUser = (user) => ({
   uid: user.uid,
-  email: user.email
+  email: user.email,
+  displayName: user.displayName,
+  phoneNumber: user.phoneNumber,
+  photoURL: user.photoURL,
 });
 
 export default function useFirebaseAuth() {
   const [authUser, setAuthUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  const clear = () => {
+    setAuthUser(null);
+    setLoading(true);
+  };
+
   const authStateChanged = async (authState) => {
     if (!authState) {
-      setAuthUser(null)
-      setLoading(false)
+      setAuthUser(null);
+      setLoading(false);
       return;
     }
 
-    setLoading(true)
+    setLoading(true);
     var formattedUser = formatAuthUser(authState);
-    setAuthUser(formattedUser);    
+    setAuthUser(formattedUser);
     setLoading(false);
   };
 
-// listen for Firebase state change
+  // listen for Firebase state change
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(authStateChanged);
     return () => unsubscribe();
   }, []);
 
+  const signInWithEmailAndPassword = ({ email, password }) =>
+    signIn(email, password);
+
+  const createUserWithEmailAndPassword = ({
+    email,
+    password,
+    displayName = "Milca",
+    phoneNumber = "6465458888",
+  }) =>
+    signUp(email, password)
+      .then(() => updateUser({ displayName, phoneNumber, photoURL: "" }))
+      .then(() => emailVerification())
+      .then(() => auth.currentUser);
+
+  const signOut = () => auth.signOut().then(clear);
+
   return {
     authUser,
-    loading
+    loading,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    signOut,
+    clear,
   };
 }
