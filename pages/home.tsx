08a39397620e7bb838ffa32d51d96a2303e66764
milcab <milcarodriguez23@gmail.com>
2022-05-25 19:49:00 -0400

    build issues

--- pages/home.tsx
+++ pages/home.tsx
@@ -10,17 +10,17 @@ const Home: NextPage = () => {
   const { authUser } = useFirebaseAuth();
 
   return (
-    <div className="flex items-center items-stretch">
+    <div className="flex items-center items-stretch flex-col md:flex-row">
       <Link href={"/money/selectRecepients?action=send"}>
         <a
-          className={`bg-blue-dark text-white text-xl antialiased font-bold flex justify-center items-center h-screen flex-1 p-12`}
+          className={`bg-blue-dark text-white text-xl antialiased font-bold flex justify-center items-center min-h-[200px] md:h-screen flex-1 p-12`}
         >
           Send Money
         </a>
       </Link>
       <Link href={"/money/selectRecepients?action=request"}>
         <a
-          className={`bg-blue-medium text-white text-xl antialiased font-bold flex justify-center items-center h-screen flex-1 p-12`}
+          className={`bg-blue-medium text-white text-xl antialiased font-bold flex justify-center items-center min-h-[200px] md:h-screen flex-1 p-12`}
         >
           Request Money
         </a>
