9e7d416c1028e89cc12d4600f2d7e01a70f701db
milcab <milcarodriguez23@gmail.com>
2022-05-25 19:31:38 -0400

    adding secrets here

--- firebase/app.ts
+++ firebase/app.ts
@@ -16,15 +16,29 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+const config = {
+  NEXT_PUBLIC_FIREBASE_API_KEY: "AIzaSyDkoD9pAZK6rzu6BqIAgcDaxspTDpwJuBY",
+  NEXT_PUBLIC_FIREBASE_APP_ID: "75471273840",
+  NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN:
+    "milca-transfer-njit-project.firebaseapp.com",
+  NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: "G-WRJZWV84F1",
+  NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "75471273840",
+  NEXT_PUBLIC_FIREBASE_PROJECT_ID: "milca-transfer-njit-project",
+  NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET:
+    "milca-transfer-njit-project.appspot.com",
+  NEXT_PUBLIC_FIREBASE_DATABASE_URL:
+    "https://milca-transfer-njit-project-default-rtdb.firebaseio.com/",
+};
+
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: config.NEXT_PUBLIC_FIREBASE_API_KEY,
+  appId: config.NEXT_PUBLIC_FIREBASE_APP_ID,
+  authDomain: config.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: config.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  measurementId: config.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: config.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  projectId: config.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: config.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
 };
 
 // Initialize Firebase
