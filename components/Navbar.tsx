af633131a1874f7520ea4fa16ba0b82b81d3b895
milcab <milcarodriguez23@gmail.com>
2022-05-25 19:45:31 -0400

    build issues

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,5 @@
-/* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from "react";
-import Link from "next/Link";
+import Link from "next/link";
 import { Popover, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import { ChevronDownIcon } from "@heroicons/react/solid";
--- pages/money/selectRecepients.tsx
+++ pages/money/selectRecepients.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import { forwardRef } from "react";
 import Head from "next/head";
 import Image from "next/image";
-import Link from "next/Link";
+import Link from "next/link";
 import useFirebaseAuth, {
   useContacts,
   useTransaction,
